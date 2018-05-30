cc1b6f4ea7babe38211a53c425b3a614f127f39f
gavinlen <gavin_len@163.com>
2018-05-30 17:14:58 +0800

    a

---
+++ spring-boot/spring-boot-base/src/test/java/xyz/ieden/base/BaseTest.java
@@ -0,0 +1,22 @@
+package xyz.ieden.base;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Map;
+
+/**
+ * @author ThinkPad
+ * @date Created by 2018/5/23 9:15
+ */
+public class BaseTest {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(BaseTest.class);
+
+    @Test
+    public void testMap() {
+        Map<String, >
+    }
+
+}
